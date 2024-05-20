c9eff991475a75234ad7a46d6f8c1adb8ed743ba
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-05-20 21:08:59 +0200

    update

--- proxy.js
+++ proxy.js
@@ -1,24 +1,29 @@
 const express = require('express');
-const https = require('https'); 
-const http = require('http'); 
-const {URL} = require('url'); 
+const https = require('https');
+const http = require('http');
+const { URL } = require('url');
 
-const app = express(); 
+const app = express();
 
+// Middleware to set CORS headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-}); 
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 app.get('/proxy', (req, res) => {
-    const {target} = res.query; 
-    if(!target) {
-        res.status(400).send('Bad Request: Target URL is required');
-        return; 
+    const { target } = req.query;
+    if (!target) {
+        res.status(400).send('Bad Request: Target URL is required.');
+        return;
     }
-
 
     const targetUrl = new URL(target);
     const protocol = targetUrl.protocol === 'https:' ? https : http;
@@ -33,11 +38,9 @@ app.get('/proxy', (req, res) => {
     }).on('error', (err) => {
         res.status(500).send('Internal Server Error: ' + err.message);
     });
+});
 
-
-})
-
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Proxy server is runniong on port ${PORT}`);
-}); 
+    console.log(`Proxy server is running on port ${PORT}`);
+});
