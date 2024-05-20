84176d58dec3359ec4b23b3933ed67e9037560ce
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-05-20 21:03:06 +0200

    update

--- proxy.js
+++ proxy.js
@@ -5,6 +5,13 @@ const {URL} = require('url');
 
 const app = express(); 
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+}); 
+
 app.get('/proxy', (req, res) => {
     const {target} = res.query; 
     if(!target) {
